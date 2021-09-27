8bd87aa700296db211e648d205d2d2183d97d8cb
CarpinettiMatias <carpi417@gmail.com>
2021-09-27 10:29:18 -0300

    menu
    menu

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
-import React, { Component } from "react";
-import "./styles.css";
+import React from "react";
+import NavBar from "./Components/NavBar.js";
 
-// Components
-import NavBar from "./components/NavBar";
 
-// + 1 elemento JSX retornado:
-// envolver en un div o utilizar un Fragment <> </>
-// Embeber codigo de js: entre {} llaves
-
-const styles = {
-  paragraph: {
-    fontSize: 25,
-    color: "orange",
-    backgroundColor: "green"
-  }
-};
-
-let name = "Daniel";
-let city = "Buenos Aires";
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <NavBar />
-        <h1 style={{ backgroundColor: "yellow", border: "5px solid black" }}>
-          Nombre del usuario: {name}
-        </h1>
-        <p>Edad del usuario: {31 + 5}</p>
-        <p style={styles.paragraph}>Ciudad de usuario: {city}</p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+          <div>
+              <div>
+                  <NavBar/>
+              </div>
+              <div>
+                  <ul>
+                    <li>Producto</li>
+                    <li>Producto</li>
+                    <li>Producto</li>
+                    <li>Producto</li>
+                    <li>Producto</li>
+                    <li>Producto</li>
+                    <li>Producto</li>
+                    <li>Producto</li>
+                  </ul>
+              </div>
+          </div>
+    </div>
+   
+  );
 }
 
 export default App;
-
---
+++ src/Component/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavBar = () => {
+    return( 
+        <div>
+        <h1>Luna</h1>
+        <ul>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Productos</a></li>
+            <li><a href="">Contacto</a></li>
+            <li><a href="">Ayuda</a></li>
+        </ul>
+        </div>
+    );
+}
+export default NavBar;
